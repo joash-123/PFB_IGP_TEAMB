c07ee19ab9d7bd3eb97ef1a61edf6846231545e5
joash lee <s10220929@connect.np.edu.sg>
2022-08-02 15:21:40 +0800

    deleted stuff

--- test.py
+++ test.py
@@ -24,15 +24,7 @@ data = r.json()
 print(data)
 
 
-fp = Path.cwd()/"currency.csv"
-fp.touch()
-with fp.open(mode = "w", encoding="UTF-8", newline="") as file:
-    writer =  csv.writer(file)
-    # write header
-    writer.writerow(["Forex weekly prices", "From USD", "To SGD", "Last Refreshed", "Time Zone (UTC)"])
-    # iterate over data to write data in csv file
-    for data in currency["data"]:
-        writer.writerow([data["date"], data["value"],data["value"],data["value"],data["value"]])
 
+print(data.items())
 
 print("hello")        
