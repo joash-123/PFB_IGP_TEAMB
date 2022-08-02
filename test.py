42d3ea9b91d186289786626e4ada4aa6c543d2c9
joash lee <s10220929@connect.np.edu.sg>
2022-08-02 14:17:21 +0800

    hi

--- test.py
+++ test.py
@@ -2,3 +2,17 @@ print("hello world")
 print("hello world again")
 
 print("test again")
+
+from pathlib import Path
+import csv
+import requests
+
+api_key = "BRB9X55YUVV8G334"
+import requests
+
+# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={api_key}'
+
+
+response = requests.get(url)
+print(response)
